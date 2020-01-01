8f02d03f6c2f54119b59590b6cf6a101526ac856
Sane <tretyakvl@gmail.com>
2020-01-01 19:35:55 +0300

    Styles mobile nav menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,10 +5,29 @@ import logo from '../../images/logo.svg'
 
 const Header = () => (
   <header className={style.header}>
-    <img src={logo} alt="Shortly logo"/>
+    <img src={logo} alt='Shortly logo' />
+    <button>
+      Open menu
+    </button>
+    <nav className={style.header__navigation}>
+      <ul className={style.header__links}>
+        <li>
+          <a href=''>Features</a>
+        </li>
+        <li>
+          <a href=''>Pricing</a>
+        </li>
+        <li>
+          <a href=''>Resources</a>
+        </li>
+      </ul>
+      <ul className={style.header__links}>
+        <li><a href=''>Login</a></li>
+        <li><a href=''>Sing Up</a></li>
+      </ul>
+    </nav>
   </header>
 )
 console.log(style)
-
 
 export default Header
