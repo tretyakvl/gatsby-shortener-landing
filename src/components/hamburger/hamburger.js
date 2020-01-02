83733422dd804788c88f4da80f338205d7e68979
Sane <tretyakvl@gmail.com>
2020-01-02 21:54:54 +0300

    Changes hamburger icon

--- src/components/hamburger/hamburger.js
+++ src/components/hamburger/hamburger.js
@@ -8,26 +8,27 @@ const Path = props => (
     fill='transparent'
     strokeWidth='3'
     stroke='var(--textColor500)'
+    initial='closed'
     {...props}
   />
 )
 
-const Hamburger = ({ isOpen }) => {
+const Hamburger = ({ isOpen, onClick }) => {
   return (
     <motion.button
       className={style.hamburger}
-      onClick={() => {}}
+      onClick={onClick}
     >
-      <svg width='23' height='23' viewBox='0 0 23 23'>
+      <svg width='25' height='25' viewBox='0 0 25 25'>
         <Path
           variants={{
-            closed: { d: 'M 2 2.5 L 20 2.5' },
-            open: { d: 'M 3 16.5 L 17 2.5' }
+            closed: { d: 'M 2 4 L 23 4' },
+            open: { d: 'M 3 22 L 22 3' }
           }}
           animate={isOpen ? 'open' : 'closed'}
         />
         <Path
-          d='M 2 9.423 L 20 9.423'
+          d='M 2 12.5 L 23 12.5'
           variants={{
             closed: { opacity: 1 },
             open: { opacity: 0 }
@@ -37,8 +38,8 @@ const Hamburger = ({ isOpen }) => {
         />
         <Path
           variants={{
-            closed: { d: 'M 2 16.346 L 20 16.346' },
-            open: { d: 'M 3 2.5 L 17 16.346' }
+            closed: { d: 'M 2 21 L 23 21' },
+            open: { d: 'M 3 3 L 22 22' }
           }}
           animate={isOpen ? 'open' : 'closed'}
         />
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,33 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Hamburger from '../hamburger/hamburger.js'
 
 import style from './header.module.css'
 import logo from '../../images/logo.svg'
 
-const Header = () => (
-  <header className={style.header}>
-    <img src={logo} alt='Shortly logo' />
-    <Hamburger />
-    <nav className={style.header__navigation}>
-      <ul className={style.header__links}>
-        <li>
-          <a href=''>Features</a>
-        </li>
-        <li>
-          <a href=''>Pricing</a>
-        </li>
-        <li>
-          <a href=''>Resources</a>
-        </li>
-      </ul>
-      <ul className={style.header__links}>
-        <li><a href=''>Login</a></li>
-        <li><a href=''>Sing Up</a></li>
-      </ul>
-    </nav>
-  </header>
-)
+const Header = () => {
+  const [isOpen, setOpen] = useState(false)
+
+  return (
+    <header className={style.header}>
+      <img src={logo} alt='Shortly logo' />
+      <Hamburger isOpen={isOpen} onClick={() => setOpen(!isOpen)} />
+      <nav className={style.header__navigation}>
+        <ul className={style.header__links}>
+          <li>
+            <a href=''>Features</a>
+          </li>
+          <li>
+            <a href=''>Pricing</a>
+          </li>
+          <li>
+            <a href=''>Resources</a>
+          </li>
+        </ul>
+        <ul className={style.header__links}>
+          <li><a href=''>Login</a></li>
+          <li><a href=''>Sing Up</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
 console.log(style)
 
 export default Header
