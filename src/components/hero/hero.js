811ecef2a518bdbc17fa50f01e8e9f785f328c5a
Sane <tretyakvl@gmail.com>
2020-01-06 20:32:25 +0300

    FInishes hero desktop styles

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react"
 
-import style from './hero.module.css'
+import style from "./hero.module.css"
 
 const Hero = () => {
   return (
     <section className={style.hero}>
-      <h2 className={style.hero__header}>More than just shorter links</h2>
-      <p className={style.hero__text}>
+      <div className={style.hero__container}>
+        <h2 className={style.hero__header}>More than just shorter links</h2>
+        <p className={style.hero__text}>
           Build your brand’s recognition and get detailed insights on how your
           links are performing.
-      </p>
-      <a>Get Started</a>
+        </p>
+        <a>Get Started</a>
+      </div>
     </section>
   )
 }
