f920cb719ab37f54d989d23f247e07845875c7af
Sane <tretyakvl@gmail.com>
2020-01-07 20:28:54 +0300

    Remakes hero layout with flex

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,10 +1,17 @@
-import React from "react"
+import React from 'react'
 
-import style from "./hero.module.css"
+import illustration from '../../images/illustration-working.svg'
+import style from './hero.module.css'
 
 const Hero = () => {
   return (
     <section className={style.hero}>
+      <img
+        src={illustration}
+        width='733'
+        height='482'
+        alt='Illustration of a woman working on a computer'
+      />
       <div className={style.hero__container}>
         <h2 className={style.hero__header}>More than just shorter links</h2>
         <p className={style.hero__text}>
