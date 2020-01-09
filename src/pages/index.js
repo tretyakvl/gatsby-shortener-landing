ea8b2174038d48e75ac6503585dc6a7ee5f82b2d
Sane <tretyakvl@gmail.com>
2020-01-09 16:09:17 +0300

    Adds query for index page data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,58 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout/layout'
 import Seo from '../components/seo'
 import Hero from '../components/hero/hero'
 
 const IndexPage = ({ data }) => {
-  console.log(data)
+  console.log(data.indexYaml)
 
   return (
     <Layout>
       <Seo title='Shortly' />
-      <h1 className='visually-hidden'>Shortly URL shortening</h1>
+      <h1 className='visually-hidden'>{data.indexYaml.title}</h1>
       <Hero />
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    indexYaml {
+      title
+      sections {
+        hero {
+          title
+          desc
+          illustration {
+            publicURL
+          }
+          cta {
+            text
+            to
+          }
+        }
+        features {
+          tite
+          desc
+          list {
+            item {
+              title
+              desc
+            }
+          }
+        }
+        start {
+          title
+          cta {
+            text
+            to
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
