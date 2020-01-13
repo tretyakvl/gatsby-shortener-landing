151739ffbd9cb82741b0b2f18dac6dc3fffc9290
Sane <tretyakvl@gmail.com>
2020-01-13 18:36:36 +0300

    Makes shortener input controlled component

--- src/components/shortener/shortener.js
+++ src/components/shortener/shortener.js
@@ -1,20 +1,32 @@
-import React from 'react'
+/**
+ * TODO:
+ *
+ * Handle errors, 404 and 500
+ */
+
+import React, { useState } from 'react'
 
 import style from './shortener.module.css'
 
 const Shortener = () => {
-  const handleSumbit = (event) => {
-    event.preventDefault()
-    console.log(event)
-  }
+  const [urlToShorten, setUrlToShorten] = useState('')
 
   return (
     <section className={style.shortener}>
       <form
         className={style.shortener__form}
-        onSubmit={handleSumbit}
+        onSubmit={event => {
+          event.preventDefault()
+          console.log(urlToShorten)
+        }}
       >
-        <input type='text' id='shortener-email' placeholder='Shorten a link here...' />
+        <input
+          type='text'
+          id='shortener-url'
+          placeholder='Shorten a link here...'
+          value={urlToShorten}
+          onChange={event => setUrlToShorten(event.target.value)}
+        />
         <button type='submit'>Shorten it</button>
       </form>
     </section>
@@ -22,3 +34,11 @@ const Shortener = () => {
 }
 
 export default Shortener
+
+async function fetchShortenedUrl (url) {
+  fetch('https://rel.ink/api/links/', {
+    method: 'POST',
+    mode: 'cors',
+    body: JSON.stringify({ url })
+  }).then(response => console.log(response))
+}
