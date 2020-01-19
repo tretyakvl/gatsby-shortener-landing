d7cc8e409c0e91dc410293600750faea79cd214d
Sane <tretyakvl@gmail.com>
2020-01-19 17:24:00 +0300

    Finishes features desktop styles

--- src/components/features/features.js
+++ src/components/features/features.js
@@ -4,21 +4,23 @@ import style from './features.module.css'
 
 const Features = ({ title, desc, list }) => {
   return (
-    <section className={style.features}>
-      <h2 className='visually-hidden'>Shortly features</h2>
-      <div className={style.features__main}>
-        <h3>{title}</h3>
-        <p>{desc}</p>
-      </div>
-      <ul className={style.features__list}>
-        {list.map(({ item }, i) => (
-          <li key={i}>
-            <h3>{item.title}</h3>
-            <p>{item.desc}</p>
-          </li>
-        ))}
-      </ul>
-    </section>
+    <div className={style.container}>
+      <section className={style.features}>
+        <h2 className='visually-hidden'>Shortly features</h2>
+        <div className={style.features__main}>
+          <h3>{title}</h3>
+          <p>{desc}</p>
+        </div>
+        <ul className={style.features__list}>
+          {list.map(({ item }, i) => (
+            <li key={i}>
+              <h3>{item.title}</h3>
+              <p>{item.desc}</p>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
   )
 }
 
