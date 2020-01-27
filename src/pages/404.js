9421f51f0caea9cfcd7fe88c09c1229a1c11ee0d
Sane <tretyakvl@gmail.com>
2020-01-27 20:49:08 +0300

    Adds 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,14 +2,31 @@ import React from 'react'
 
 import Layout from '../components/layout/layout'
 import Seo from '../components/seo'
-import Logo from '../components/logo/logo'
 
-const NotFoundPage = () => (
-  <Layout>
-    <Seo title='404: Not found' />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+import illustration from '../images/illustration-404.svg'
+import style from './404.module.css'
+
+const NotFoundPage = () => {
+  console.log(style)
+  return (
+    <Layout>
+      <Seo title='404: Not found' />
+      <main className={style.notFound}>
+        <h1>Page not found</h1>
+        <div className={style.notFound__illustration}>
+          <img
+            src={illustration}
+            width='796'
+            height='1074'
+            alt='Illustration of a woman reaching down'
+          />
+          <span>
+            404
+          </span>
+        </div>
+      </main>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
