6173febfe771fc38f33752ccba5a9cde48aa80a8
Sane <tretyakvl@gmail.com>
2020-01-29 18:10:55 +0300

    Fixes build issues with localstorage

--- src/hooks/usePersistentState.js
+++ src/hooks/usePersistentState.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react'
 
 const usePersistentState = (key, intialValue) => {
-  const storedString = localStorage.getItem(key)
+  const storedString =
+    typeof window !== 'undefined' && window.localStorage.getItem(key)
+
   const [value, setValue] = useState(
     storedString ? JSON.parse(storedString) : intialValue
   )
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value))
+    typeof window !== 'undefined' &&
+      window.localStorage.setItem(key, JSON.stringify(value))
   }, [key, value])
 
   return [value, setValue]
